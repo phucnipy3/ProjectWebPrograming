8ea8402db478991c7d26a4a65234bd958c1b1d87
Phúc Nguyễn Huỳnh <phucnipy3@gmail.com>
2019-11-28 10:42:10 +0700

    Related product cart index view

---
+++ MVC/Controllers/CartController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC.Controllers
+{
+    public class CartController : ApplicationController
+    {
+        // GET: Cart
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
