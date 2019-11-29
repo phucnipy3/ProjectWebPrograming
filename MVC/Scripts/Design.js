88efb66322c7f3cac861eb8e303055b80452d0d9
Phúc Nguyễn Huỳnh <phucnipy3@gmail.com>
2019-11-29 12:47:16 +0700

    Làm design chút chút

--- MVC/Scripts/Design.js
+++ MVC/Scripts/Design.js
@@ -91,3 +91,108 @@ $(document).ready(function(){
 	
 	
 });
+
+function CloseDialog(str)
+{
+    document.getElementById(str).style.visibility = "hidden";
+return true;
+}
+function ShowDialog(str)
+{
+    document.getElementById(str).style.visibility = "visible";
+return true;
+}
+function ResetSizeText()
+{
+    document.getElementById("EnterText").value = "Hello";
+    document.getElementById("EnterColor").value = "#000000";
+    document.getElementById("Size").value = "30";
+    document.getElementById("EnterFont").value = "arial";
+}
+function SetFont(st)
+{
+    document.getElementById("EnterFont").value = st;
+    return true;
+}
+function ChangeColor(str)
+{
+    document.getElementById("CaseColor").style.backgroundColor = str;
+    document.getElementById("CaseColor").style.backgroundImage = "";
+    return true;
+}
+function ChangeHeight()
+{
+    document.getElementById("CaseColor").style.height = document.getElementById("Img").height + "px";
+    return true;
+}
+function ChangeCase(str)
+{
+    $("#Img").attr("src", "/Resource/OpTrong/" + str);
+return true;
+}
+function ThemHinh(str)
+{
+    return AddImg("/Resource/HinhMau/"+str);
+}
+function AddImg(str)
+{
+	var img = document.createElement("div");
+    img.style.position = "absolute";
+    img.style.left = "100px";
+    img.style.top = "120px";
+    img.classList.add("UIDraggable");
+    img.style.border = "1px dashed #72C3AB";
+    img.style.width = "200px";
+    img.style.height = "200px";
+    img.style.backgroundSize = "100% 100%";
+    img.style.backgroundRepeat = "no-repeat";
+    currentElement = $(img);
+    currentElement.css("background-image","url("+str+")");
+    $(".UIDraggable").css("border","0px dashed #72C3AB");
+    $("#DesignArea").append(img);
+    CloseDialog('SizeText');
+    ShowDialog('SizeImg');
+    ResetSizeImg();
+    return true;
+}
+function ThemHinhTuLink()
+{
+	var str = document.getElementById("LinkAnh").value.trim();
+    document.getElementById("LinkAnh").value = "";
+    return AddImg(str);
+}
+function ResetSizeImg()
+{
+    document.getElementById("ChieuCao").value = "200";
+    document.getElementById("ChieuRong").value = "200";
+    return true;
+}
+function MoChiTiet()
+{
+    window.open('trangbanhang/thongtin.html');
+}
+function ChangeBackground(str)
+{
+    document.getElementById("CaseColor").style.backgroundImage = "url('/Resource/WaterColor/" + str + "')";
+    document.getElementById("CaseColor").style.backgroundColor = "#FFFFFF";
+    return true;
+}
+function loginClick()
+{
+    CloseDialog("login");
+inforlogin();
+}
+function signinClick()
+{
+    CloseDialog("signin");
+    alert("Bạn đã đăng ký thành công!");
+return true;
+}
+function inforlogin()
+{
+    alert("Bạn đã đăng nhập thành công!");
+}
+function GioHangClick()
+{
+    alert("Giỏ hàng của bạn đang trống");
+}
