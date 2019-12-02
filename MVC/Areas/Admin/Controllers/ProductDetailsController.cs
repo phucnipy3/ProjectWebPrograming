9eb8af0572bb10315bd6c0709092af837dacf4b6
Phúc Nguyễn Huỳnh <phucnipy3@gmail.com>
2019-12-02 16:51:05 +0700

    Sửa sơ sơ

--- MVC/Areas/Admin/Controllers/ProductDetailsController.cs
+++ MVC/Areas/Admin/Controllers/ProductDetailsController.cs
@@ -7,45 +7,82 @@ using System.Web;
 using System.Web.Mvc;
 using MVC.Models;
 using MVC.Areas.Admin.Models;
-
+using PagedList;
 namespace MVC.Areas.Admin.Controllers
 {
     [Authorize(Roles ="Admin,Employee")]
     public class ProductDetailsController : ApplicationController,IAdminController<ProductDetail>
     {
-        // GET: Admin/ProductDetails
-        public ActionResult Index()
-        {
-            return View();
-        }
+        
         public ActionResult Add(ProductDetail model)
         {
-            throw new NotImplementedException();
+            if (ModelState.IsValid)
+            {
+
+                if (ProductDetailHelper.AddProductDetail(model))
+                {
+                    return Content("success");
+                }
+                else
+                {
+                    ModelState.AddModelError("", "Existing User ID!");
+                    return PartialView("Add");
+                }
+            }
+            else
+            {
+                return PartialView("Add");
+            }
         }
 
         public ActionResult Delete(DeleteViewModel model)
         {
             throw new NotImplementedException();
         }
+        public ActionResult Delete(DeleteProductDetailViewModel model)
+        {
+            if (ProductDetailHelper.DeleteProductDetail(model.ProductID,model.ProductCategoryID))
+                return Content("success");
+            return Content("failure");
+        }
+        public ActionResult DeleteTransfer(int? productID,int?  productCategoryID)
+        {
+            return PartialView("ConfirmDelete", new DeleteProductDetailViewModel() { ProductCategoryID = (int)productCategoryID,ProductID = (int)productID });
+        }
 
         public ActionResult DeleteTransfer(int? id)
         {
             throw new NotImplementedException();
         }
-        
+
         public ActionResult Index(string searchString, int page = 1, int pageSize = 10)
         {
-            throw new NotImplementedException();
+            IEnumerable<ProductDetail> models;
+            if (String.IsNullOrEmpty(searchString))
+                models = ProductDetailHelper.GetProductDetails();
+            else
+            {
+                models = ProductDetailHelper.GetProductDetails(searchString);
+            }
+            ViewBag.SearchString = searchString;
+            return View(models.OrderByDescending(x => x.ProductID).ToPagedList(page, pageSize));
         }
 
         public ActionResult Update(ProductDetail model)
         {
-            throw new NotImplementedException();
+            if (ProductDetailHelper.AddProductDetail(model))
+                return Content("success");
+            return Content("failure");
         }
 
         public ActionResult UpdateTransfer(int? id)
         {
             throw new NotImplementedException();
+        }
+        public ActionResult UpdateTransfer(int? productID, int? productCategoryID)
+        {
+            ProductDetail model = ProductDetailHelper.GetProductDetails().FirstOrDefault();
+            return PartialView("Update", model);
         }
     }
 }
