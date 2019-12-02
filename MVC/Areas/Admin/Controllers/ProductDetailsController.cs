9072f739847e0e513f4fd53a87ff8f4a49486875
Phúc Nguyễn Huỳnh <phucnipy3@gmail.com>
2019-12-02 15:59:21 +0700

    thêm quản lý product details

---
+++ MVC/Areas/Admin/Controllers/ProductDetailsController.cs
@@ -0,0 +1,51 @@
+﻿using MVC.Areas.Admin.Code;
+using MVC.Controllers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MVC.Models;
+using MVC.Areas.Admin.Models;
+
+namespace MVC.Areas.Admin.Controllers
+{
+    [Authorize(Roles ="Admin,Employee")]
+    public class ProductDetailsController : ApplicationController,IAdminController<ProductDetail>
+    {
+        // GET: Admin/ProductDetails
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Add(ProductDetail model)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ActionResult Delete(DeleteViewModel model)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ActionResult DeleteTransfer(int? id)
+        {
+            throw new NotImplementedException();
+        }
+        
+        public ActionResult Index(string searchString, int page = 1, int pageSize = 10)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ActionResult Update(ProductDetail model)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ActionResult UpdateTransfer(int? id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ MVC/Areas/Admin/Models/DeleteProductDetailViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVC.Areas.Admin.Models
+{
+    public class DeleteProductDetailViewModel
+    {
+        public int ProductID { get; set; }
+        public int ProductCategoryID { get; set; }
+    }
+}
